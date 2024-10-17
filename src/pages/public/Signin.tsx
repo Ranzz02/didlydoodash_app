a1f9a8f6ff719c89f30813e04b3dd4cd2f7da2b7
Rasmus Räihä <rasmus.raiha@hotmail.com>
2024-10-17 16:27:51 +0300

    fixing wait-on

--- src/pages/public/Signin.tsx
+++ src/pages/public/Signin.tsx
@@ -4,108 +4,109 @@ import { API } from "@/utils/api";
 import { useAuth } from "@/context/AuthContext";
 import { toast } from "react-toastify";
 import { useAuthStore } from "@/stores/auth/store";
+import { Link } from "react-router-dom";
 
 export default function SignupPage() {
-	const [input, setInput] = useState({
-		email: "",
-		password: "",
-		rememberMe: false,
-	});
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    rememberMe: false,
+  });
 
-	const [passwordShown, setPasswordShown] = useState(false);
+  const [passwordShown, setPasswordShown] = useState(false);
 
-	const { login } = useAuth();
-	const { setTokens } = useAuthStore();
+  const { login } = useAuth();
+  const { setTokens } = useAuthStore();
 
-	const handleSubmitEvent = (e: FormEvent) => {
-		e.preventDefault();
-		if (input.email !== "" && input.password !== "") {
-			return API.post("/auth/signin", { ...input })
-				.then((response) => {
-					console.log(response);
-					login(response.data.user);
-					setTokens(response.data.tokens);
-					toast.success(`Welcome back ${response.data.user.username}`);
-				})
-				.catch(() => {
-					toast.error("Failed to signin check your credentials", {
-						position: "top-left",
-					});
-				})
-				.finally();
-		}
-		toast.error("Please fill out the form first", {
-			position: "top-left",
-		});
-	};
+  const handleSubmitEvent = (e: FormEvent) => {
+    e.preventDefault();
+    if (input.email !== "" && input.password !== "") {
+      return API.post("/auth/signin", { ...input })
+        .then((response) => {
+          console.log(response);
+          login(response.data.user);
+          setTokens(response.data.tokens);
+          toast.success(`Welcome back ${response.data.user.username}`);
+        })
+        .catch(() => {
+          toast.error("Failed to signin check your credentials", {
+            position: "top-left",
+          });
+        })
+        .finally();
+    }
+    toast.error("Please fill out the form first", {
+      position: "top-left",
+    });
+  };
 
-	const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-		const { name, type, checked, value } = e.target;
-		setInput((prev) => ({
-			...prev,
-			[name]: type === "checkbox" ? checked : value,
-		}));
-	};
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, type, checked, value } = e.target;
+    setInput((prev) => ({
+      ...prev,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
 
-	const toggleRememberMe = () => {
-		setInput((prev) => ({
-			...prev,
-			rememberMe: !input.rememberMe,
-		}));
-	};
+  const toggleRememberMe = () => {
+    setInput((prev) => ({
+      ...prev,
+      rememberMe: !input.rememberMe,
+    }));
+  };
 
-	return (
-		<div className="container">
-			<div className="form">
-				<h1>DidlydooDash</h1>
-				<h2>Welcome back!</h2>
-				<form onSubmit={handleSubmitEvent}>
-					<div className="form-field">
-						<label>Email:</label>
-						<input
-							type="email"
-							id="user-email"
-							name="email"
-							placeholder="example@mail.com"
-							aria-description="user-email"
-							aria-invalid="false"
-							onChange={handleInput}
-						/>
-					</div>
-					<div className="form-field">
-						<label>Password:</label>
-						<input
-							type={passwordShown ? "text" : "password"}
-							id="user-password"
-							name="password"
-							placeholder="Password"
-							aria-description="user-password"
-							aria-invalid="false"
-							onChange={handleInput}
-						/>
-						<img
-							src={passwordShown ? "/icons/hide.svg" : "/icons/show.svg"}
-							onClick={() => setPasswordShown(!passwordShown)}
-						/>
-					</div>
-					<div className="form-field row">
-						<div className="rememberMe" onClick={toggleRememberMe}>
-							<input
-								type="checkbox"
-								name="rememberMe"
-								checked={input.rememberMe}
-								onChange={handleInput}
-							/>
-							<label>Remember me!</label>
-						</div>
-						<a href="/forgot">Forgot password?</a>
-					</div>
-					<button className="">Submit</button>
-				</form>
-				<span>
-					Not signed up? <a href="/signup">Signup</a>
-				</span>
-			</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="form">
+        <h1>DidlydooDash</h1>
+        <h2>Welcome back!</h2>
+        <form onSubmit={handleSubmitEvent}>
+          <div className="form-field">
+            <label>Email:</label>
+            <input
+              type="email"
+              id="user-email"
+              name="email"
+              placeholder="example@mail.com"
+              aria-description="user-email"
+              aria-invalid="false"
+              onChange={handleInput}
+            />
+          </div>
+          <div className="form-field">
+            <label>Password:</label>
+            <input
+              type={passwordShown ? "text" : "password"}
+              id="user-password"
+              name="password"
+              placeholder="Password"
+              aria-description="user-password"
+              aria-invalid="false"
+              onChange={handleInput}
+            />
+            <img
+              src={passwordShown ? "/icons/hide.svg" : "/icons/show.svg"}
+              onClick={() => setPasswordShown(!passwordShown)}
+            />
+          </div>
+          <div className="form-field row">
+            <div className="rememberMe" onClick={toggleRememberMe}>
+              <input
+                type="checkbox"
+                name="rememberMe"
+                checked={input.rememberMe}
+                onChange={handleInput}
+              />
+              <label>Remember me!</label>
+            </div>
+            <a href="/forgot">Forgot password?</a>
+          </div>
+          <button className="">Submit</button>
+        </form>
+        <span>
+          Not signed up? <Link to="/signup">Signup</Link>
+        </span>
+      </div>
+    </div>
+  );
 }
--- src/pages/public/Signup.tsx
+++ src/pages/public/Signup.tsx
@@ -4,103 +4,104 @@ import { API } from "@/utils/api";
 import { useAuth } from "@/context/AuthContext";
 import { toast } from "react-toastify";
 import { useAuthStore } from "@/stores/auth/store";
+import { Link } from "react-router-dom";
 
 export default function SignupPage() {
-	const [input, setInput] = useState({
-		username: "",
-		email: "",
-		password: "",
-	});
+  const [input, setInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
-	const [passwordShown, setPasswordShown] = useState(false);
+  const [passwordShown, setPasswordShown] = useState(false);
 
-	const { login } = useAuth();
-	const { setTokens } = useAuthStore();
+  const { login } = useAuth();
+  const { setTokens } = useAuthStore();
 
-	const handleSubmitEvent = (e: FormEvent) => {
-		e.preventDefault();
-		if (input.username !== "" && input.email !== "" && input.password !== "") {
-			return API.post("/auth/signup", { ...input })
-				.then((response) => {
-					console.log(response);
-					login(response.data.user);
-					setTokens(response.data.tokens);
-					toast.success(
-						`Welcome to didlydoodash ${response.data.user.username}`
-					);
-				})
-				.catch(() => {
-					toast.error("Failed to signin check inputs", {
-						position: "top-left",
-					});
-				})
-				.finally();
-		}
-		toast.error("Please fill out the form first", {
-			position: "top-left",
-		});
-	};
+  const handleSubmitEvent = (e: FormEvent) => {
+    e.preventDefault();
+    if (input.username !== "" && input.email !== "" && input.password !== "") {
+      return API.post("/auth/signup", { ...input })
+        .then((response) => {
+          console.log(response);
+          login(response.data.user);
+          setTokens(response.data.tokens);
+          toast.success(
+            `Welcome to didlydoodash ${response.data.user.username}`
+          );
+        })
+        .catch(() => {
+          toast.error("Failed to signin check inputs", {
+            position: "top-left",
+          });
+        })
+        .finally();
+    }
+    toast.error("Please fill out the form first", {
+      position: "top-left",
+    });
+  };
 
-	const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = e.target;
-		setInput((prev) => ({
-			...prev,
-			[name]: value,
-		}));
-	};
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInput((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-	return (
-		<div className="container">
-			<div className="form">
-				<h1>DidlydooDash</h1>
-				<h2>Get started now</h2>
-				<form onSubmit={handleSubmitEvent}>
-					<div className="form-field">
-						<label>Username:</label>
-						<input
-							type="text"
-							id="user-name"
-							name="username"
-							placeholder="name"
-							aria-description="user-name"
-							aria-invalid="false"
-							onChange={handleInput}
-						/>
-					</div>
-					<div className="form-field">
-						<label>Email:</label>
-						<input
-							type="email"
-							id="user-email"
-							name="email"
-							placeholder="example@mail.com"
-							aria-description="user-email"
-							aria-invalid="false"
-							onChange={handleInput}
-						/>
-					</div>
-					<div className="form-field">
-						<label>Password:</label>
-						<input
-							type={passwordShown ? "text" : "password"}
-							id="user-password"
-							name="password"
-							placeholder="Password"
-							aria-description="user-password"
-							aria-invalid="false"
-							onChange={handleInput}
-						/>
-						<img
-							src={passwordShown ? "/icons/hide.svg" : "/icons/show.svg"}
-							onClick={() => setPasswordShown(!passwordShown)}
-						/>
-					</div>
-					<button className="">Submit</button>
-				</form>
-				<span>
-					Already signed up? <a href="/signin">Signin</a>
-				</span>
-			</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="form">
+        <h1>DidlydooDash</h1>
+        <h2>Get started now</h2>
+        <form onSubmit={handleSubmitEvent}>
+          <div className="form-field">
+            <label>Username:</label>
+            <input
+              type="text"
+              id="user-name"
+              name="username"
+              placeholder="name"
+              aria-description="user-name"
+              aria-invalid="false"
+              onChange={handleInput}
+            />
+          </div>
+          <div className="form-field">
+            <label>Email:</label>
+            <input
+              type="email"
+              id="user-email"
+              name="email"
+              placeholder="example@mail.com"
+              aria-description="user-email"
+              aria-invalid="false"
+              onChange={handleInput}
+            />
+          </div>
+          <div className="form-field">
+            <label>Password:</label>
+            <input
+              type={passwordShown ? "text" : "password"}
+              id="user-password"
+              name="password"
+              placeholder="Password"
+              aria-description="user-password"
+              aria-invalid="false"
+              onChange={handleInput}
+            />
+            <img
+              src={passwordShown ? "/icons/hide.svg" : "/icons/show.svg"}
+              onClick={() => setPasswordShown(!passwordShown)}
+            />
+          </div>
+          <button className="">Submit</button>
+        </form>
+        <span>
+          Already signed up? <Link to="/signin">Signin</Link>
+        </span>
+      </div>
+    </div>
+  );
 }
